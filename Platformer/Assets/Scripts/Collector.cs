370410be3dbdcbc05844c79682379d878603c29b
ArcaLuiNoe <seby.ro@yahoo.com>
2023-07-16 21:04:10 +0300

    Actually Fixed Lava Particles, Added Collectibles Made them unique

--- Platformer/Assets/Scripts/Collector.cs
+++ Platformer/Assets/Scripts/Collector.cs
@@ -1,19 +1,41 @@
 using UnityEditor.Experimental.GraphView;
 using System.Linq;
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using UnityEngine.Events;
 
 public class Collector : MonoBehaviour
 {
-	[SerializeField] Collectible[] _collectibles;
+	[SerializeField] List<Collectible> _collectibles;
+
+    [SerializeField] UnityEvent _onCollectionComplete;
+    TMP_Text _remainingText;
+
+    void Start()
+    {
+        _remainingText = GetComponentInChildren<TMP_Text>();
+    }
 
     void Update()
     {
+        int _countRemaining = 0;
         foreach (var collectible in _collectibles)
         {
             if (collectible.isActiveAndEnabled)
-                return;
+                _countRemaining++;
         }
 
-        Debug.Log("Got All Gems!");
+        _remainingText?.SetText(_countRemaining.ToString());
+
+        if (_countRemaining > 0)
+            return;
+
+        _onCollectionComplete.Invoke();
+    }
+
+    void OnValidate()
+    {
+        _collectibles = _collectibles.Distinct().ToList();
     }
 }
